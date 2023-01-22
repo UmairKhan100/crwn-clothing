8264e5c660e8ff9def8d0d5f1eaab8e3ab1a1783
Umair <umairzeb10@gmail.com>
2023-01-22 23:27:02 +0500

    persisted the redux store using redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -3,17 +3,21 @@ import {
   compose,
   applyMiddleware,
 } from "redux";
-// import logger from "redux-logger";
+import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
-const logger = (store) => (next) => (action) => {
-  console.log("currentState: ", store.getState());
-  next(action);
-  console.log("nextState: ", store.getState());
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
 };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [logger];
 const enhancers = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, undefined, enhancers);
+export const store = createStore(persistedReducer, undefined, enhancers);
+export const persistor = persistStore(store);
